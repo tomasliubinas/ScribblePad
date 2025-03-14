48ec91385cd149023de832d87140b08db1933561
Tomas Liubinas <t.liubinas@paysera.com>
2025-03-15 01:33:58 +0200

    Initial ScriblePad implementation

---
+++ ScriblePad/Controllers/PersistenceController.swift
@@ -0,0 +1,8 @@
+//
+//  PersistenceController.swift
+//  ScriblePad
+//
+//  Created by Tomas Liubinas Paysera on 2025-03-15.
+//
+
+import Foundation
---
+++ ScriblePad/Views/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  ScriblePad
+//
+//  Created by Tomas Liubinas Paysera on 2025-03-15.
+//
+
+import Foundation
